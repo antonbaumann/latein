6ee43011e8191864740774cb5e91a4d6ca7c6419
antonbaumann <antonbaumann4@gmail.com>
2018-01-02 19:58:03 +0100

    fixed the problem with the too big dictionary file

--- latin.py
+++ latin.py
@@ -3,8 +3,27 @@
 
 import sqlite3 as lite
 import sys
+import os
 
-con = lite.connect('res/token.sqlite')
+home = os.path.expanduser("~")
+dir_path = home + '/latein'
+db_path = home + '/latein/dict.sqlite'
+zip_path = 'res/dict.zip'
+
+if not os.path.exists(dir_path):
+	print('creating directory:', dir_path)
+	os.mkdir(dir_path)
+
+if not os.path.isfile(db_path):
+	print('On first use of this script the dictionary has to be decompressed.')
+	print('Please unzip `dict.zip` and place it in `~/latein`')	
+	sys.exit()
+
+if len(sys.argv) < 2:
+	print('missing argument.')
+	sys.exit()
+
+con = lite.connect(db_path)
 con.text_factory = bytes
 
 with con:
